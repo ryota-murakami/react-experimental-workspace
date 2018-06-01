0f125567ee27c7584a026a2c6af6f2881a5e3882
ryota-murakami <dojce1048@gmail.com>
2018-06-01 13:33:01 +0900

    fweiofweoifjiwejfiowe

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react'
+import { Container, Text } from './style'
+
+class App extends Component<{||}> {
+  render() {
+    return (
+      <Container>
+        <Text>HHHHHHHHHHwhiow</Text>
+      </Container>
+    )
+  }
+}
+
+export default App
---
+++ src/App/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export const Text = styled.div`
+  font-size: 16px;
+`
