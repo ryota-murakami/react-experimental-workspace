28dcc906715ed93d1b0cdd33efd68582f35cd6a7
ryota-murakami <dojce1048@gmail.com>
2018-06-03 03:08:21 +0900

    styled babel

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
 /* config-overrides.js */
+const rewireStyledComponents = require('react-app-rewire-styled-components')
+
 module.exports = function override(config, env) {
   if (
     config.module.rules[0] &&
@@ -6,5 +8,11 @@ module.exports = function override(config, env) {
   ) {
     config.module.rules.splice(0, 1)
   }
+
+  config = rewireStyledComponents(config, env, {
+    ssr: false,
+    displayName: true
+  })
+
   return config
 }
