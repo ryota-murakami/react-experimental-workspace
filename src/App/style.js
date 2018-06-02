a3b5faa8d73ef7f43ab073a427a7bb7f7795eb59
ryota-murakami <dojce1048@gmail.com>
2018-06-02 23:18:22 +0900

    up

--- src/App/style.js
+++ src/App/style.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export const Text = styled.div`
