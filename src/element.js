85e4b4483f6bd8243e0ea01b363be8126a010062
ryota-murakami <dojce1048@gmail.com>
2018-06-13 15:08:11 +0900

    up

--- src/element.js
+++ src/element.js
@@ -4,6 +4,14 @@ export const Text = styled.div`
   font-size: 16px;
 `
 
-export const Input = styled.input``
+export const Input = styled.input`
+  &:focus {
+    outline: none;
+  }
+`
 
-export const Button = styled.button``
+export const Button = styled.button`
+  &:focus {
+    outline: none;
+  }
+`
