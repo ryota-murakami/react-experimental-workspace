23f3ab4123d5778bb2e580c7eaf636a2ae064ab8
ryota-murakami <dojce1048@gmail.com>
2018-06-13 15:15:15 +0900

    up

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,29 @@
 // @flow
 import React, { Component } from 'react'
-import { Input, Button } from '../element'
+import { Input, Button, Text } from '../element'
 import { Container, InputSlice } from './style'
 import TodoList from './TodoList'
 import type { TodoList as TodoListType } from '../type'
 
-class App extends Component<void> {
+type State = {
+  todos: TodoListType
+}
+
+class App extends Component<void, State> {
+  state = { todos: [] }
+
   render() {
-    const todos: TodoListType = [{ id: 'idddddddddd', text: 'text valueeee' }]
     return (
       <Container>
         <InputSlice style={{ height: '30%' }}>
           <Input />
           <Button>push</Button>
         </InputSlice>
-        <TodoList data={todos} style={{ height: '70%' }} />
+        {this.state.todos.length < 1 ? (
+          <Text>let's enter todo!</Text>
+        ) : (
+          <TodoList data={this.state.todos} style={{ height: '70%' }} />
+        )}
       </Container>
     )
   }
