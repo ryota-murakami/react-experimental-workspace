840524394c985526e22151e31254d4b96ee57e53
ryota-murakami <dojce1048@gmail.com>
2018-06-15 11:33:07 +0900

    up

--- src/App/App.js
+++ src/App/App.js
@@ -3,27 +3,45 @@ import React, { Component } from 'react'
 import { Input, Button, Text } from '../element'
 import { Container, InputSlice } from './style'
 import TodoList from './TodoList'
-import type { TodoList as TodoListType } from '../type'
+import type { Todo, TodoList as TodoListType } from '../type'
+
+const ID = function() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return (
+    '_' +
+    Math.random()
+      .toString(36)
+      .substr(2, 9)
+  )
+}
 
 type State = {
-  todos: TodoListType
+  todos: TodoListType,
+  currentInput: string
 }
 
 class App extends Component<void, State> {
   inputRef: ?HTMLInputElement = React.createRef()
-  state: State = { todos: [] }
+  state: State = { todos: [], currentInput: '' }
 
   addTodo = () => {
-    // inputRefのvalue取得
-    console.log(this.inputRef.current.value)
-    // TODO styled-componentsのelementからthis.inputRef.current.valueどうやって取得すんの
+    console.log(this.state.currentInput)
+    const text = this.state.currentInput
+    const newTodo: Todo = { id: ID(), text: text }
+    this.setState({ todos: [...this.state.todos, newTodo], currentInput: '' })
+  }
+
+  _onChange = e => {
+    this.setState({ currentInput: e.target.value })
   }
 
   render() {
     return (
       <Container>
         <InputSlice style={{ height: '30%' }}>
-          <input ref={this.inputRef} />
+          <Input value={this.state.currentInput} onChange={this._onChange} />
           <Button onClick={this.addTodo}>push</Button>
         </InputSlice>
         {this.state.todos.length < 1 ? (
