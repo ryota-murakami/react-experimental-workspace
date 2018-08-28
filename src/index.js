2f216ba85390207ebd9ffecd70acc3da31e563c9
ryota-murakami <dojce1048@gmail.com>
2018-08-28 22:57:06 +0900

    Routes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
 // @flow
-import React, { Fragment } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import Loadable from 'react-loadable'
-import { Loading } from './shared/Loading'
-import { BrowserRouter, Route } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import Routes from './Routes'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import { reducer } from './reducer'
-
-/* prettier-ignore */
-const App = Loadable({ loader: () => import('./pages/App'/* webpackChunkName: "App" */), loading: Loading })
-/* prettier-ignore */
-const Todo = Loadable({
-  loader: () => import('./pages/Todo'/* webpackChunkName: "Todo" */),
-  loading: Loading
-})
-/* prettier-ignore */
-const Rerender = Loadable({
-  loader: () => import('./pages/Rerender'/* webpackChunkName: "Rerender" */),
-  loading: Loading
-})
-/* prettier-ignore */
-const Form = Loadable({
-  loader: () => import('./pages/Form'/* webpackChunkName: "Form" */),
-  loading: Loading
-})
-/* prettier-ignore */
-const Anime = Loadable({
-  loader: () => import('./pages/Anime'/* webpackChunkName: "Anime" */),
-  loading: Loading
-})
 
 const store = createStore(
   reducer,
@@ -40,15 +15,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Fragment>
-        <Route exact path="/" component={App} />
-        <Route exact path="/todo" component={Todo} />
-        <Route exact path="/rerender" component={Rerender} />
-        <Route exact path="/form" component={Form} />
-        <Route exact path="/anime" component={Anime} />
-      </Fragment>
-    </BrowserRouter>
+    <Routes />
   </Provider>,
   // $FlowIssue
   document.getElementById('root')
