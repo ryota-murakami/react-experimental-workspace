e063dcf79c222501359fa7e21fd8c74c373ddbf1
ryota-murakami <dojce1048@gmail.com>
2018-08-29 19:25:24 +0900

    Link

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -2,7 +2,13 @@
 import React, { Component } from 'react'
 import { pure } from 'recompose'
 import { Link } from 'react-router-dom'
-import { Container, LinkContainer } from './index.style'
+import {
+  Container,
+  LinkWrapper,
+  Header,
+  HeaderContainer,
+  LinkContainer
+} from './index.style'
 
 type Props = {}
 
@@ -10,14 +16,19 @@ class App extends Component<Props> {
   render() {
     return (
       <Container>
+        <HeaderContainer>
+          <Header>React Workspace</Header>
+        </HeaderContainer>
         <LinkContainer>
-          <Link to="/todo">Todo</Link>
-        </LinkContainer>
-        <LinkContainer>
-          <Link to="/form">Form</Link>
-        </LinkContainer>
-        <LinkContainer>
-          <Link to="/anime">CSS Animation</Link>
+          <LinkWrapper>
+            <Link to="/todo">・Todo</Link>
+          </LinkWrapper>
+          <LinkWrapper>
+            <Link to="/form">・Form</Link>
+          </LinkWrapper>
+          <LinkWrapper>
+            <Link to="/anime">・CSS Animation</Link>
+          </LinkWrapper>
         </LinkContainer>
       </Container>
     )
--- src/pages/App/index.style.js
+++ src/pages/App/index.style.js
@@ -7,7 +7,27 @@ export const Container = styled.div`
   align-items: center;
 `
 
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Header = styled.h1`
+  font-size: 44px;
+  font-weight: bold;
+`
+
 export const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const LinkWrapper = styled.div`
+  font-size: 24px;
   margin-top: 10px;
   margin-bottom: 10px;
 `
