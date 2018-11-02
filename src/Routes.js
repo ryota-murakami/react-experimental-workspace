658804b1a4e9d6f5e458a81d473a735b183b9957
ryota-murakami <dojce1048@gmail.com>
2018-11-02 20:46:32 +0900

    react-loadable to Suspense

--- src/Routes.js
+++ src/Routes.js
@@ -1,36 +1,23 @@
-// @flow
-import React, { Fragment } from 'react'
-import Loadable from 'react-loadable'
+import React, { Suspense, lazy } from 'react'
 import { Loading } from './shared/Loading'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-/* prettier-ignore */
-const Index = Loadable({ loader: () => import('./pages/Index'/* webpackChunkName: "Index" */), loading: Loading })
-/* prettier-ignore */
-const Todo = Loadable({
-  loader: () => import('./pages/Todo'/* webpackChunkName: "Todo" */),
-  loading: Loading
-})
-/* prettier-ignore */
-const Form = Loadable({
-  loader: () => import('./pages/Form'/* webpackChunkName: "Form" */),
-  loading: Loading
-})
-/* prettier-ignore */
-const Anime = Loadable({
-  loader: () => import('./pages/Anime'/* webpackChunkName: "Anime" */),
-  loading: Loading
-})
+const Index = lazy(() => import('./pages/Index'))
+const Todo = lazy(() => import('./pages/Todo'))
+const Form = lazy(() => import('./pages/Form'))
+const Anime = lazy(() => import('./pages/Anime'))
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Fragment>
-        <Route exact path="/" component={Index} />
-        <Route exact path="/todo" component={Todo} />
-        <Route exact path="/form" component={Form} />
-        <Route exact path="/anime" component={Anime} />
-      </Fragment>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/todo" component={Todo} />
+          <Route exact path="/form" component={Form} />
+          <Route exact path="/anime" component={Anime} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   )
 }
