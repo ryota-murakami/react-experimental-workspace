e794ecbdd5552c017b7307949aa928c6dc7bed26
ryota-murakami <dojce1048@gmail.com>
2018-11-04 00:04:33 +0900

    add context

--- src/pages/Context/View.js
+++ src/pages/Context/View.js
@@ -6,9 +6,28 @@ const layout = css`
   width: 100%;
   height: 100%;
   display: flex;
+  font-size: 36px;
+  font-weight: bold;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`
+
+const buttonStyle = css`
+  height: 70px;
+  width: 150px;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: bold;
+
+  &:focus {
+    outline: 0;
+  }
+
+  &:active {
+    box-shadow: inset -2px -2px 3px rgba(255, 255, 255, 0.6),
+      inset 2px 2px 3px rgba(0, 0, 0, 0.6);
+  }
 `
 
 function View() {
@@ -18,7 +37,9 @@ function View() {
         {context => (
           <Fragment>
             <h1>count: {context.count}</h1>
-            <button onClick={context.increment}>increment</button>
+            <button className={buttonStyle} onClick={context.increment}>
+              increment
+            </button>
           </Fragment>
         )}
       </MyContext.Consumer>
