f6ebc8f23c0eb674c258b88108f6ba90c25b96f8
ryota-murakami <dojce1048@gmail.com>
2018-11-07 12:34:41 +0900

    btn height

--- src/pages/Context/View.js
+++ src/pages/Context/View.js
@@ -2,6 +2,14 @@ import React, { Fragment } from 'react'
 import { MyContext } from './index'
 import { css } from 'emotion'
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+import classNames from 'classnames'
+
+const styleds = {
+  root: {
+    height: 58
+  }
+}
 
 const layout = css`
   width: 100%;
@@ -14,14 +22,21 @@ const layout = css`
   align-items: center;
 `
 
-function View() {
+function View(props) {
+  const { classes, className } = props
+
   return (
     <div className={layout}>
       <MyContext.Consumer>
         {context => (
           <Fragment>
             <h1>count: {context.count}</h1>
-            <Button variant="outlined" size="large" onClick={context.increment}>
+            <Button
+              variant="outlined"
+              size="large"
+              className={classNames(classes.root, className)}
+              onClick={context.increment}
+            >
               increment
             </Button>
           </Fragment>
@@ -31,4 +46,4 @@ function View() {
   )
 }
 
-export default View
+export default withStyles(styleds)(View)
