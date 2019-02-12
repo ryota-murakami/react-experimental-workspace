d799b7e81a6a3a1319c88c83b9c0488e74601423
ryota-murakami <dojce1048@gmail.com>
2019-02-12 13:49:34 +0900

    refactor routes sight

--- src/Routes.js
+++ src/Routes.js
@@ -2,26 +2,42 @@ import React, { Suspense, lazy } from 'react'
 import { Loading } from './components/Loading'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const Index = lazy(() => import('./pages')) // http://localhost:3000/
-const Todo = lazy(() => import('./pages/Todo')) // // http://localhost:3000/todo
-const Form = lazy(() => import('./pages/Form'))
-const Anime = lazy(() => import('./pages/Anime'))
-const Context = lazy(() => import('./pages/Context'))
-const Hooks = lazy(() => import('./pages/Hooks'))
-const UseReducer = lazy(() => import('./pages/Hooks/UseReducer'))
-
 const Routes = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
         <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/todo" component={Todo} />
-          <Route exact path="/form" component={Form} />
-          <Route exact path="/anime" component={Anime} />
-          <Route exact path="/context" component={Context} />
-          <Route exact path="/hooks" component={Hooks} />
-          <Route exact path="/hooks/usereducer" component={UseReducer} />
+          <Route exact path="/" component={lazy(() => import('./pages'))} />
+          <Route
+            exact
+            path="/todo"
+            component={lazy(() => import('./pages/Todo'))}
+          />
+          <Route
+            exact
+            path="/form"
+            component={lazy(() => import('./pages/Form'))}
+          />
+          <Route
+            exact
+            path="/anime"
+            component={lazy(() => import('./pages/Anime'))}
+          />
+          <Route
+            exact
+            path="/context"
+            component={lazy(() => import('./pages/Context'))}
+          />
+          <Route
+            exact
+            path="/hooks"
+            component={lazy(() => import('./pages/Hooks'))}
+          />
+          <Route
+            exact
+            path="/hooks/usereducer"
+            component={lazy(() => import('./pages/Hooks/UseReducer'))}
+          />
           <Route
             component={() => (
               <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }} /* eslint-disable-line prettier/prettier */>
