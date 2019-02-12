d6cbc1a0adb9ed0ac6b882d6aa09e2b52598678a
ryota-murakami <dojce1048@gmail.com>
2019-02-12 14:54:40 +0900

    done usereducer

--- src/pages/Hooks/UseReducer/index.js
+++ src/pages/Hooks/UseReducer/index.js
@@ -1,5 +1,43 @@
-import React from 'react'
+// @flow
+import React, { useReducer } from 'react'
+import { css } from 'emotion'
+
+const layout = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  font-size: 36px;
+  font-weight: bold;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const initialState = { count: 0 }
+
+const reducer = (state, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
 
 export default function UseReducer() {
-  return <h1>UseReducer</h1>
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div className={layout}>
+      <h1>UseReducer</h1>
+      <div>
+        <h1>Count: {state.count}</h1>
+        <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      </div>
+    </div>
+  )
 }
