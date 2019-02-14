568c40655b5e90b3c3e06e7c0004bf210f89eb59
ryota-murakami <dojce1048@gmail.com>
2019-02-14 21:41:54 +0900

    tmp emotion

--- src/pages/Hooks/UseReducer/index.js
+++ src/pages/Hooks/UseReducer/index.js
@@ -1,10 +1,14 @@
 // @flow
 import React, { useReducer } from 'react'
-import { css } from '@emotion/core'
+import { css, jsx } from '@emotion/core'
 
 const container = css`
   width: 100%;
   height: 100%;
+`
+
+const layout = css`
+  margin: 0 auto;
   display: flex;
   font-size: 36px;
   font-weight: bold;
@@ -32,11 +36,13 @@ export default function UseReducer() {
 
   return (
     <div className={container}>
-      <h1>UseReducer</h1>
-      <div>
-        <h1>Count: {state.count}</h1>
-        <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-        <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <div className={layout}>
+        <h1>UseReducer</h1>
+        <div>
+          <h1>Count: {state.count}</h1>
+          <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+          <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+        </div>
       </div>
     </div>
   )
