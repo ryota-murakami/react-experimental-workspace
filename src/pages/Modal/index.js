dd69c74214ae3340d8c4af91663967799f58897b
ryota-murakami <dojce1048@gmail.com>
2019-07-03 21:27:13 +0900

    add

--- src/pages/Modal/index.js
+++ src/pages/Modal/index.js
@@ -14,58 +14,62 @@ const items = [
 export default () => {
   return (
     <Layout>
-      <h1>Modal</h1>
-      <Downshift
-        onCange={selection =>
-          alert(
-            selection ? `You selected ${selection.value}` : 'Selection Cleared'
-          )
-        }
-        itemToString={item => (item ? item.value : '')}
-      >
-        {({
-          getInputProps,
-          getItemProps,
-          getLabelProps,
-          getMenuProps,
-          isOpen,
-          inputValue,
-          highlightedIndex,
-          selectedItem
-        }) => (
-          <div>
-            <label {...getLabelProps()}>Enter a fruit</label>
-            <input {...getInputProps()} />
-            <ul {...getMenuProps()}>
-              {isOpen
-                ? items
-                    .filter(
-                      item => !inputValue || item.value.includes(inputValue)
-                    )
-                    .map((item, index) => (
-                      <li
-                        {...getItemProps({
-                          key: item.value,
-                          index,
-                          item,
-                          style: {
-                            backgroundColor:
-                              highlightedIndex === index
-                                ? 'lightgray'
-                                : 'while',
-                            fontWeight:
-                              selectedItem === item ? 'bold' : 'normal'
-                          }
-                        })}
-                      >
-                        {item.value}
-                      </li>
-                    ))
-                : null}
-            </ul>
-          </div>
-        )}
-      </Downshift>
+      <h1 className="header">Modal</h1>
+      <div className="select-container">
+        <Downshift
+          onCange={selection =>
+            alert(
+              selection
+                ? `You selected ${selection.value}`
+                : 'Selection Cleared'
+            )
+          }
+          itemToString={item => (item ? item.value : '')}
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            getLabelProps,
+            getMenuProps,
+            isOpen,
+            inputValue,
+            highlightedIndex,
+            selectedItem
+          }) => (
+            <div>
+              <label {...getLabelProps()}>Enter a fruit</label>
+              <input {...getInputProps()} />
+              <ul {...getMenuProps()}>
+                {isOpen
+                  ? items
+                      .filter(
+                        item => !inputValue || item.value.includes(inputValue)
+                      )
+                      .map((item, index) => (
+                        <li
+                          {...getItemProps({
+                            key: item.value,
+                            index,
+                            item,
+                            style: {
+                              backgroundColor:
+                                highlightedIndex === index
+                                  ? 'lightgray'
+                                  : 'while',
+                              fontWeight:
+                                selectedItem === item ? 'bold' : 'normal'
+                            }
+                          })}
+                        >
+                          {item.value}
+                        </li>
+                      ))
+                  : null}
+              </ul>
+            </div>
+          )}
+        </Downshift>
+      </div>
     </Layout>
   )
 }
