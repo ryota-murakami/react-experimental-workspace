0a505bdfe25209f565e12bdf47b06372aca34d46
ryota-murakami <dojce1048@gmail.com>
2019-12-23 01:00:30 +0900

    ommit devtools

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import Router from './Router'
 import './index.css'
 import { reducer } from './reducer'
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
