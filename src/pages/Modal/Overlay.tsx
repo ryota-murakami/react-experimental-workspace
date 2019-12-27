f06b63788e2975c94fb3052220230764f942ada0
ryota-murakami <dojce1048@gmail.com>
2019-12-27 23:10:08 +0900

    tmp

---
+++ src/pages/Modal/Overlay.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Overlay: React.FC = () => {
+  return <div>Overlay</div>
+}
+
+export default Overlay
--- src/pages/Modal/index.tsx
+++ src/pages/Modal/index.tsx
@@ -3,13 +3,19 @@ import { Layout, Row } from './style'
 import { Button } from '@material-ui/core'
 
 const Modal: React.FC = () => {
+  function openModal(): void {
+    // @TODO show layer under the modal that would be cover whole screen
+    // @TODO should be close Modal when you click overlayscreencover
+    // @TODO show Modal itself that contain contents, close Button
+  }
+
   return (
     <Layout>
       <Row>
         <h1>Modal</h1>
       </Row>
       <Row>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={openModal}>
           Open
         </Button>
       </Row>
