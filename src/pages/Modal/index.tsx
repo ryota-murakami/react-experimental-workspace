708ab100c3de44d733392da069fc6ded445d7a7f
ryota-murakami <dojce1048@gmail.com>
2019-12-27 11:38:13 +0900

    tmp

---
+++ src/pages/Modal/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout, Row } from './style'
+
+const Modal: React.FC = () => {
+  return (
+    <Layout>
+      <Row>
+        <h1>Modal</h1>
+      </Row>
+    </Layout>
+  )
+}
+
+export default Modal
---
+++ src/pages/Modal/style.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+
+// flex container for Row
+export const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+// flex items for Layout
+export const Row = styled.div`
+  display: flex;
+  justify-content: center;
+`
