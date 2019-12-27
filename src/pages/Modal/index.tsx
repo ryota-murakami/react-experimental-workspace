694e8dbf05da28f53d96a461ed48c0773894c77c
ryota-murakami <dojce1048@gmail.com>
2019-12-27 11:51:09 +0900

    btn

--- src/pages/Modal/index.tsx
+++ src/pages/Modal/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Layout, Row } from './style'
+import { Button } from '@material-ui/core'
 
 const Modal: React.FC = () => {
   return (
     <Layout>
       <Row>
         <h1>Modal</h1>
+      </Row>
+      <Row>
+        <Button variant="contained" color="primary">
+          Open
+        </Button>
       </Row>
     </Layout>
   )
