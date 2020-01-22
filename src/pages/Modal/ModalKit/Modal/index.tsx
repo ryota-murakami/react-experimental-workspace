3daf8333e070baf78c01c41645eda3661a1815b7
ryota-murakami <dojce1048@gmail.com>
2020-01-22 23:23:33 +0900

    styled modal

--- src/pages/Modal/ModalKit/Modal/index.tsx
+++ src/pages/Modal/ModalKit/Modal/index.tsx
@@ -22,17 +22,35 @@ const Layout = styled.div<LayoutProps>(
     marginLeft: '-300px',
     top: '50%',
     marginTop: '-180px',
-    padding: '32px',
-    flexDrection: 'colmun'
+    flexDirection: 'column'
   },
   props => ({ display: props.isOpen ? 'flex' : 'none' })
 )
 
-const Head = styled.div``
+const Head = styled.div`
+  width: 568px;
+  height: 32px;
+  padding: 16px;
+  font-size: 24px;
+  font-weight: 400;
+`
 
-const Content = styled.div``
+const Content = styled.div`
+  border-top: 1px solid darkgray;
+  border-bottom: 1px solid darkgray;
+  width: 568px;
+  height: 292px;
+  padding: 16px;
+  overflow: scroll;
+`
 
-const Bottom = styled.div``
+const Bottom = styled.div`
+  width: 568px;
+  height: 36px;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-end;
+`
 
 interface Props {
   isOpen: ModalState['isOpen']
@@ -43,7 +61,22 @@ const Modal: React.FC<Props> = ({ isOpen, closeModal }) => {
   return (
     <Layout isOpen={isOpen}>
       <Head>Modal</Head>
-      <Content>I'm content.</Content>
+      <Content>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+        <p>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+          Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </p>
+        <p>
+          Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+          magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+          ullamcorper nulla non metus auctor fringilla.
+        </p>
+      </Content>
       <Bottom>
         <Button variant="contained" color="primary" onClick={closeModal}>
           Close
