4bb9e3f82d6fc0aab20c9dc38b501d799a58c7da
ryota-murakami <dojce1048@gmail.com>
2020-01-22 21:08:27 +0900

    layout

--- src/pages/Modal/ModalKit/Modal/index.tsx
+++ src/pages/Modal/ModalKit/Modal/index.tsx
@@ -1,36 +1,38 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx } from '@emotion/core'
+import styled from '@emotion/styled'
 import { ModalState } from '../../index'
 // @TODO show Modal itself that contain contents, close Button
+
+interface LayoutProps {
+  isOpen: boolean
+}
+
+const Layout = styled.div<LayoutProps>(
+  {
+    zIndex: 3,
+    width: '600px',
+    height: '360px',
+    backgroundColor: 'white',
+    margin: 'auto 0',
+    fontSize: 20,
+    position: 'absolute',
+    left: '50%',
+    marginLeft: '-300px',
+    top: '50%',
+    marginTop: '-180px',
+    padding: '32px'
+  },
+  props => ({ display: props.isOpen ? 'block' : 'none' })
+)
 
 interface Props {
   isOpen: ModalState['isOpen']
 }
 
 const Modal: React.FC<Props> = ({ isOpen }) => {
-  const visibility = isOpen ? 'block' : 'none'
-
-  return (
-    <div
-      css={{
-        zIndex: 3,
-        width: '200px',
-        height: '200px',
-        backgroundColor: 'white',
-        margin: 'auto 0',
-        fontSize: 20,
-        display: visibility,
-        position: 'absolute',
-        left: '50%',
-        marginLeft: '-100px',
-        top: '50%',
-        marginTop: '-100px'
-      }}
-    >
-      Modal
-    </div>
-  )
+  return <Layout isOpen={isOpen}>Modal</Layout>
 }
 
 export default Modal
