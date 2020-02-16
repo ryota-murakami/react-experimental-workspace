9b13f6c5d23b5ee5aa9fb5d5ee6b9a919f0d650d
ryota-murakami <dojce1048@gmail.com>
2020-02-16 22:13:09 +0900

    tmp

--- src/pages/Perf/index.tsx
+++ src/pages/Perf/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Container, Row } from './style'
 
 const Perf: React.FC = () => {
-  return <div>Perf</div>
+  return (
+    <Container>
+      <Row>
+        <h1>aaaaa</h1>
+      </Row>
+      <Row>
+        <h2>iojoiji</h2>
+      </Row>
+    </Container>
+  )
 }
 
 export default Perf
---
+++ src/pages/Perf/style.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Row = styled.div`
+  width: 100%;
+  height: 200px;
+`
