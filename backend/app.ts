8404638842421111fb7147be34a98562fd0082c1
ryota-murakami <dojce1048@gmail.com>
2020-08-22 21:05:37 +0900

    mes

--- backend/app.ts
+++ backend/app.ts
@@ -3,7 +3,7 @@ import Express, { Request, Response } from 'express'
 const app = Express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello AAAAAAAAAAAAAAAAAAAAAP')
+  res.send('Backend Server Working!')
 })
 
 export default app
