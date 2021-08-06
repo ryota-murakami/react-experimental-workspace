63ad1b34469ee64fbca51b28419b9f7f3e353f26
ryota-murakami <dojce1048@gmail.com>
2021-08-06 22:59:29 +0900

    up

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const injectDevServer = require('@cypress/react/plugins/react-scripts')
 
 // ***********************************************************
