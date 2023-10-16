8172a240897194e1c72c19dcad0f4535bb26379f
ryota-murakami <dojce1048@gmail.com>
2023-10-16 18:35:35 +0900

    up

--- generators/pages/index.cjs
+++ generators/pages/index.cjs
@@ -1,26 +1,23 @@
-const path = require('path')
-const fs = require('fs')
-
 /**
  *
  * @type {import('plop').PlopGenerator}
  */
 module.exports = {
-  description: 'Pages Generator',
-  prompts: [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'page name',
-    },
-  ],
   actions: () => {
     return [
       {
-        type: 'add',
         path: 'src/pages/{{properCase name}}/index.tsx',
         templateFile: 'generators/pages/index.tsx.hbs',
+        type: 'add',
       },
     ]
   },
+  description: 'Pages Generator',
+  prompts: [
+    {
+      name: 'name',
+      message: 'page name',
+      type: 'input',
+    },
+  ],
 }
