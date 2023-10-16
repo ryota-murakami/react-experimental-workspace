dea95e01f9ec50281f691ab9dc57f9cd68ecb5f0
ryota-murakami <dojce1048@gmail.com>
2023-10-16 15:52:08 +0900

    implmement page templete generator

---
+++ generators/pages/index.cjs
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+/**
+ *
+ * @type {import('plop').PlopGenerator}
+ */
+module.exports = {
+  description: 'Pages Generator',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'page name',
+    },
+  ],
+  actions: () => {
+    return [
+      {
+        type: 'add',
+        path: 'src/pages/{{properCase name}}/index.tsx',
+        templateFile: 'generators/pages/index.tsx.hbs',
+      },
+    ];
+  },
+};
