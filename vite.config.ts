1cf535f818799c0d420273f8b5d25f74fbfa9f85
ryota-murakami <dojce1048@gmail.com>
2023-10-16 21:42:22 +0900

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [react(), EnvironmentPlugin(['REACT_APP_TEXT'])],
+  plugins: [react()],
   publicDir: 'public',
   server: {
     host: true,
