45c08c48e5551b9a279fd51a95fceae046b1ae22
ryota-murakami <dojce1048@gmail.com>
2023-10-16 19:56:06 +0900

    Add Link to Generated Page in Index.jsx

--- generators/pages/index.cjs
+++ generators/pages/index.cjs
@@ -10,6 +10,13 @@ module.exports = {
         templateFile: 'generators/pages/index.tsx.hbs',
         type: 'add',
       },
+      {
+        path: 'src/pages/index.jsx',
+        pattern: /(.*)(<main>[\s\S]*)(<\/main>)/m,
+        template:
+          '$1$2    <Link to="/{{lowerCase name}}">{{properCase name}}</Link>\n$3',
+        type: 'modify',
+      },
     ]
   },
   description: 'Pages Generator',
