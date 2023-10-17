d1dd6e4dafeb31d0090cd35a42c78c1de8d7c01c
ryota-murakami <dojce1048@gmail.com>
2023-10-17 19:47:22 +0900

    up

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
-interface Props {}
+import type { SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
-const Search: React.FC<Props> = () => {
-  return <div>Search</div>
+type Input = {
+  q: string
+}
+
+const Search: React.FC = () => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    watch,
+  } = useForm<Input>()
+  const onSubmit: SubmitHandler<Input> = (data) => console.log(data)
+
+  return (
+    <div className="container h-screen grid place-content-center">
+      <section className="grid place-content-center">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="site-search">Search the site:</label>
+          <input type="search" id="site-search" name="q" />
+
+          <button>Search</button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default Search
