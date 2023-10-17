5421246e5713b5465e2956b7da6d1666cc7a4402
ryota-murakami <dojce1048@gmail.com>
2023-10-17 20:45:18 +0900

    up

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -12,7 +12,6 @@ const Search: React.FC = () => {
     formState: { errors },
     handleSubmit,
     register,
-    watch,
   } = useForm<Input>()
   const onSubmit: SubmitHandler<Input> = (data) => console.log(data)
 
@@ -20,11 +19,14 @@ const Search: React.FC = () => {
     <Theme>
       <div className="container h-screen grid place-content-center">
         <section className="grid place-content-center">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="site-search">Search the site:</label>
-            <input type="search" id="site-search" name="q" />
-
-            <button>Search</button>
+          <form className="flex gap-4" onSubmit={handleSubmit(onSubmit)}>
+            <input type="search" {...register('q')} />
+            <button
+              className="text-xl px-4 py-2 font-semibold bg-white text-slate-700 dark:bg-slate-700 dark:text-white rounded-md shadow-sm ring-1 ring-slate-900/5 border-indigo-500 dark:border-sky-500 border-2 border-solid"
+              type="submit"
+            >
+              Search
+            </button>
           </form>
         </section>
       </div>
