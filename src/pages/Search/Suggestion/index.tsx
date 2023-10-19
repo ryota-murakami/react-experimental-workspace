a01171cf0154472256c57eecf5ba2893e5f6f2d5
ryota-murakami <dojce1048@gmail.com>
2023-10-19 15:02:48 +0900

    Add new Suggestion component with CSS styling

---
+++ src/pages/Search/Suggestion/index.tsx
@@ -0,0 +1,11 @@
+import type React from 'react'
+import { memo } from 'react'
+
+interface Props {}
+
+const Suggestion: React.FC<Props> = memo(() => {
+  return
+})
+Suggestion.displayName = 'Suggestion'
+
+export default Suggestion
