1ac5769c0a70d93ddf5e8aaaef9052e37f41d6d9
ryota-murakami <dojce1048@gmail.com>
2023-10-30 08:55:25 +0900

    fix container

--- src/components/PageContainer.ts
+++ src/components/PageContainer.ts
@@ -1,5 +1,5 @@
 import classed from 'classed-components'
 
 export const PageContainer = classed.div(
-  'container min-h-screen mx-auto px-4 flex flex-col',
+  'sm:container min-h-screen mx-auto flex flex-col',
 )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,12 +9,5 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-    screens: {
-      // max-sm	@media not all and (min-width: 640px) { ... }
-      // max-md	@media not all and (min-width: 768px) { ... }
-      // max-lg	@media not all and (min-width: 1024px) { ... }
-      // max-xl	@media not all and (min-width: 1280px) { ... }
-      // max-2xl @media not all and (min-width: 1536px) { ... }
-    },
   },
 }
