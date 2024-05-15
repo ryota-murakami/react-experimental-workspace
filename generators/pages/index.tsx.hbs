b11f5911cc958f0458000ef36c46fdddca54ac3e
Ryota Murakami <dojce1048@gmail.com>
2024-05-16 05:29:58 +0900

    up

--- generators/pages/index.tsx.hbs
+++ generators/pages/index.tsx.hbs
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Header from '../../components/Header'
-import { Page } from '../../components/Page'
+import Header from '@/components/Header'
+import { Page } from '@/components/Page'
 
 interface Props {}
 
--- src/pages/ContextMenu/index.tsx
+++ src/pages/ContextMenu/index.tsx
@@ -1,17 +1,52 @@
 import React from 'react'
+import { ContextMenuItem, useContextMenu } from 'use-context-menu'
 
-import Header from '../../components/Header'
-import { Page } from '../../components/Page'
+import Header from '@/components/Header'
+import { Page } from '@/components/Page'
 
 interface Props {}
 
+import 'use-context-menu/styles.css'
+
+const selectOne = () => {
+  console.log('selectOne')
+}
+
+const selectTwo = () => {
+  console.log('selectTwo')
+}
+
+const selectThree = () => {
+  console.log('selectThree')
+}
+
 const ContextMenu: React.FC<Props> = () => {
+  const { contextMenu, onContextMenu, onKeyDown } = useContextMenu(
+    <>
+      <ContextMenuItem onSelect={selectOne}>One</ContextMenuItem>
+      <ContextMenuItem onSelect={selectTwo}>Two</ContextMenuItem>
+      <ContextMenuItem onSelect={selectThree}>Three</ContextMenuItem>
+    </>,
+  )
+
   return (
     <Page.Container>
       <Header>
         <Header.H1>ContextMenu</Header.H1>
       </Header>
-      <div className="w-full grid place-content-center"></div>
+      <div className="w-full grid place-content-center">
+        <>
+          <button
+            onClick={onContextMenu}
+            onContextMenu={onContextMenu}
+            onKeyDown={onKeyDown}
+            tabIndex={0}
+          >
+            right-click me
+          </button>
+          {contextMenu}
+        </>
+      </div>
     </Page.Container>
   )
 }
